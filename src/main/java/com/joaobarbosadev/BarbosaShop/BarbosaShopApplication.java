1abf313008b111f46e922801217b0144ccb58276
João <contato.jsbarbosa@gmail.com>
2025-01-01 16:18:58 -0300

    :tada: init: Initial commit

---
+++ src/main/java/com/joaobarbosadev/BarbosaShop/BarbosaShopApplication.java
@@ -0,0 +1,13 @@
+package com.joaobarbosadev.BarbosaShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbosaShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbosaShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaobarbosadev/BarbosaShop/BarbosaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaobarbosadev.BarbosaShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbosaShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
