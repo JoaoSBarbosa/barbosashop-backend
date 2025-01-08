0295ca179a69216ed43f74e37df9d78bf0d0e058
João <contato.jsbarbosa@gmail.com>
2025-01-07 21:56:44 -0300

    :sparkles: feat: Service, Repository and Controller for Product

---
+++ src/main/java/com/joaobarbosadev/BarbosaShop/controllers/ProductController.java
@@ -0,0 +1,25 @@
+package com.joaobarbosadev.BarbosaShop.controllers;
+
+import com.joaobarbosadev.BarbosaShop.entities.Product;
+import com.joaobarbosadev.BarbosaShop.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<Page<Product>> getAllProducts(Pageable pageable) {
+        Page<Product> page = productService.findAll(pageable);
+        return ResponseEntity.ok(page);
+    }
+}
